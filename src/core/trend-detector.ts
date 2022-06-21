b88ebf4e6680cb3a70b252079f2c77c9429f8427
Yumeng <xym6336@gmail.com>
2022-06-21 23:59:09 +0200

    Update trend-detector.ts

--- src/core/trend-detector.ts
+++ src/core/trend-detector.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Yumeng Xue
  * @Date: 2022-06-20 15:13:53
- * @LastEditTime: 2022-06-20 19:45:13
+ * @LastEditTime: 2022-06-21 23:58:11
  * @LastEditors: Yumeng Xue
  * @Description: sweep line algorithm for trend deterction
  * @FilePath: /trend-mixer/src/core/trend-detector.ts
@@ -9,12 +9,29 @@
 import { Line } from "./defs/line";
 import { Trend } from "./defs/trend";
 
-function computeAllMaximalGroups(lines: Line[], duration: [number, number]): Trend[] {
-    const trends: Trend[] = [];
+function getRangeIndices(lines: Line[], duration: [number, number]): [number, number] { // suppose the lines are aligned
     const [minX, maxX] = duration;
-    const [minY, maxY] = [lines[0][0].y, lines[0][lines[0].length - 1].y];
-    return trends;
+    let minIndex = -1, maxIndex = -1;
+    for (let i = 0; i < lines[0].length; ++i) {
+        if (lines[0][i].x >= minX) {
+            minIndex = i;
+            break;
+        }
+    }
+    for (let i = lines[0].length - 1; i >= 0; --i) {
+        if (lines[0][i].x <= maxX) {
+            maxIndex = i;
+            break;
+        }
+    }
+
+    if (minIndex === -1 || maxIndex === -1) {
+        throw new Error(`cannot find range indices for duration [${minX}, ${maxX}]`);
+    }
+
+    return [minIndex, maxIndex];
 }
+
 
 /**
  * @param lines: lines to be detected
@@ -24,11 +41,30 @@ function computeAllMaximalGroups(lines: Line[], duration: [number, number]): Tre
  * @returns: detected trends
  * @description: sweep line algorithm for trend deterction
  */
-function trendDetect(lines: Line[], duration: [number, number], range: [number, number]): Trend[] {
+function computeAllMaximalGroups(lines: Line[], duration: [number, number], range: [number, number], epsilon: number): Trend[] {
     const trends: Trend[] = [];
     const [minX, maxX] = duration;
     const [minY, maxY] = range;
+    const [minIndex, maxIndex] = getRangeIndices(lines, duration);
 
+    // scan through the line set to find all maximal ε-connected sets at the start time point
+    const legalLines = lines.filter(line => line[minIndex].y >= minY && line[maxIndex].y <= maxY);
+    legalLines.sort((a, b) => a[0].y - b[0].y);
+    let lastBreak = 0;
+    const epsilonConnectedLineSets = [];
+    for (let i = 1; i < lines.length; ++i) {
+        const line = lines[i];
+        if (line[i].y - line[i - 1].y > epsilon) {
+            epsilonConnectedLineSets.push(line.slice(lastBreak, i));
+            lastBreak = i;
+        }
+    }
+    // for all epsilon-connected sets
+    for (let epsilonConnectedLineSet of epsilonConnectedLineSets) {
+        for (let i = 0; i < epsilonConnectedLineSet.length; ++i) {
+
+        }
+    }
     return trends;
 }
 
