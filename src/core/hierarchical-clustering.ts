a6f29ca33eab8674b93bd977cdbb763bfa1924a0
Yumeng <xym6336@gmail.com>
2023-03-28 02:49:47 +0200

    faster

--- src/core/hierarchical-clustering.ts
+++ src/core/hierarchical-clustering.ts
@@ -59,18 +59,22 @@ export class Hierarchical {
         this.data = data;
         const n = data.length;
         const nodes: ClusterNode[] = data.map((v, i) => new ClusterNode(v, null, null, -1, i, 1, [i]));
-        const distances: Record<string, number> = {};
+        //const distances: Record<string, number> = {};
+        if (n <= this.k) {
+            this.labels = Array(n).fill(0);
+            return;
+        }
+        const distances = new Array(n * 2 - 1).fill(0).map(() => new Array(n * 2 - 1).fill(null));
         this.labels = Array(n).fill(-1);
-        let currentClustId = -1;
+        let currentClustId = data.length;
         const priorityQueue = new FlatQueue<number[]>();
 
         console.time("hierarchical tree preprocessing");
         for (let i = 0; i < n - 1; i++) {
             for (let j = i + 1; j < n; j++) {
                 const dKey = [nodes[i].id, nodes[j].id];
-                const dKeyStr = `${nodes[i].id},${nodes[j].id}`;
-                distances[dKeyStr] = overlapCoefficientDistance(nodes[i].aggregateSet, nodes[j].aggregateSet);
-                priorityQueue.push(dKey, distances[dKeyStr]);
+                distances[nodes[i].id][nodes[j].id] = overlapCoefficientDistance(nodes[i].aggregateSet, nodes[j].aggregateSet);
+                priorityQueue.push(dKey, distances[nodes[i].id][nodes[j].id]);
             }
         }
         console.timeEnd("hierarchical tree preprocessing");
@@ -109,30 +113,29 @@ export class Hierarchical {
                 node1.count + node2.count,
                 node1.binIdList.concat(node2.binIdList)
             );
-            currentClustId -= 1;
+            currentClustId += 1;
             nodes.splice(part2, 1);
             nodes.splice(part1, 1); // 一定要先del索引较大的
 
             for (let i = 0; i < nodes.length; i++) {
                 const dKey = [nodes[i].id, new_node.id];
-                const dKeyStr = `${nodes[i].id},${new_node.id}`;
                 let accumulateDist = 0;
                 if (new_node.left !== null) {
-                    let subDKeyStr = `${nodes[i].id},${new_node.left.id}`;
-                    if (!(subDKeyStr in distances)) {
-                        subDKeyStr = `${new_node.left.id},${nodes[i].id}`;
+                    let tmpDist = distances[nodes[i].id][new_node.left.id];
+                    if (tmpDist === null) {
+                        tmpDist = distances[new_node.left.id][nodes[i].id];
                     }
-                    accumulateDist += distances[subDKeyStr] * nodes[i].binIdList.length * new_node.left.binIdList.length;
+                    accumulateDist += tmpDist * nodes[i].binIdList.length * new_node.left.binIdList.length;
                 }
                 if (new_node.right !== null) {
-                    let subDKeyStr = `${nodes[i].id},${new_node.right.id}`;
-                    if (!(subDKeyStr in distances)) {
-                        subDKeyStr = `${new_node.right.id},${nodes[i].id}`;
+                    let tmpDist = distances[nodes[i].id][new_node.right.id];
+                    if (tmpDist === null) {
+                        tmpDist = distances[new_node.right.id][nodes[i].id];
                     }
-                    accumulateDist += distances[subDKeyStr] * nodes[i].binIdList.length * new_node.right.binIdList.length;
+                    accumulateDist += tmpDist * nodes[i].binIdList.length * new_node.right.binIdList.length;
                 }
-                distances[dKeyStr] = accumulateDist / (nodes[i].binIdList.length * new_node.binIdList.length);
-                priorityQueue.push(dKey, distances[dKeyStr]);
+                distances[nodes[i].id][new_node.id] = accumulateDist / (nodes[i].binIdList.length * new_node.binIdList.length);
+                priorityQueue.push(dKey, distances[nodes[i].id][new_node.id]);
             }
 
             nodes.push(new_node);
